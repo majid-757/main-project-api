1a8db5a6f9c258c43436a4a82b750a67315e9b56
unknown <majid.asadollahi95@gmail.com>
2023-11-16 17:37:38 +0330

    improving-result

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,13 +6,32 @@ import './Input.css'
 
 const Input = (props) => {
     let inputElement = null
+    const inputClass = ['input-element']
+
+
+
+    if (props.invalid && props.used) {
+        inputClass.push('invalid')
+    }
+
+
 
     switch (props.inputtype) {
         case 'input':
-            inputElement = <input className="input-element" {...props} />
+            inputElement = <input 
+            className={inputClass.join(' ')}
+            {...props.elementConfig} 
+            value={props.value} 
+            onChange={props.change}
+            />
             break
         default:
-            inputElement = <input className="input-element" {...props} />
+            inputElement = <input 
+            className={inputClass.join(' ')}
+            {...props.elementConfig} 
+            value={props.value}
+            onChange={props.change}
+            />
     }
 
     return (
--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "../../axios-orders";
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
@@ -7,14 +8,138 @@ import './Account.css'
 
 
 class Account extends React.Component {
+    state = {
+        form: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'name...',
+                },
+                value: '',
+                validation: {
+                    required: true,
+
+                },
+                valid: false,
+                used: false
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'email...',
+                },
+                value: '',
+                validation: {
+                    required: true,
+
+                },
+                valid: false,
+                used: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'password...',
+                },
+                value: '',
+                validation: {
+                    required: true,
+
+                },
+                valid: false,
+                used: false
+            }
+        }
+    }
+
+
+    checkValidation = (value, roles) => {
+        let isValid = false
+
+        if (roles.required) {
+            isValid = value.trim() !== ''
+        }
+
+        return isValid
+    }
+
+
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        const formData = {
+
+        }
+
+        for (let item in this.state.form) {
+            formData[item] = this.state.form[item].value
+        }
+
+        axios.post('/account.json', formData)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+
+
+    inputChangeHandler = (event, inputElement) => {
+        const updatedForm =  {
+            ...this.state.form
+        }
+
+        const updatedElement = {
+            ...updatedForm[inputElement]
+        }
+
+        updatedElement.value = event.target.value
+
+        updatedElement.valid = this.checkValidation(updatedElement.value, updatedElement.validation)
+
+        updatedElement.used = true
+
+
+
+        updatedForm[inputElement] = updatedElement
+
+        this.setState({ form: updatedForm })
+    }
+
+
+
     render() {
+        const elementsArray = []
+
+        for(let item in this.state.form) {
+            elementsArray.push({
+                id: item,
+                config: this.state.form[item]
+            })
+        }
+
         return (
             <div className="account">
                 <h2>Account</h2>
-                <form>
-                    <Input type="text" placeholder="Name..." />
-                    <Input type="text" placeholder="Email..." />
-                    <Input type="password" placeholder="Password..." />
+                <form onSubmit={this.submitHandler}>
+                    {elementsArray.map((item) => {
+                        return (
+                            <Input 
+                            key={item.id} 
+                            elementType={item.config.elementType} 
+                            elementConfig={item.config.elementConfig}
+                            value={item.config.value}
+                            invalid={!item.config.valid}
+                            used={item.config.used}
+                            change={(event) => this.inputChangeHandler(event, item.id)}
+                            />
+                        )
+                    })}
                     <Button btnType="form">Submit</Button>
                 </form>
             </div>
